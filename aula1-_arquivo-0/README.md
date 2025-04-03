b1dc5a66fc883fe57d0398a2bb4977d3d372673a
Pedro Aguiar <115799705+Tiep7@users.noreply.github.com>
2025-04-03 17:08:12 -0300

    Conversor de moedas

---
+++ aula1-_arquivo-0/README.md
@@ -0,0 +1,6 @@
+# AULA1 _Arquivo 0
+
+A Pen created on CodePen.
+
+Original URL: [https://codepen.io/Pedro-Aguiar-the-lessful/pen/ByaGVjO](https://codepen.io/Pedro-Aguiar-the-lessful/pen/ByaGVjO).
+
---
+++ aula1-_arquivo-0/src/script.js
@@ -0,0 +1,7 @@
+let valor = prompt("Digite o valor que deseja converter: ");
+let berry = 0.04;
+let dolar = 5.706;
+let Euro = 6.172;    
+alert("R$" + berry * valor);
+alert("R$" + dolar * valor);
+alert("R$" + Euro * valor);
